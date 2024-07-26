c8b0bc094f06f332cd0a17b7a36c5f1cd27186f4
alicansadeler <alicansadeler@gmail.com>
2024-07-26 16:27:55 +0300

    main dosyasinda proje senaryosu düzenlendi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,30 +4,33 @@ import org.example.enums.Plan;
 
 public class Main {
     public static void main(String[] args) {
+        // Company oluşturma (5 geliştirici kapasitesi ile)
+        Company workintech = new Company(1, "Workintech", 1000000.0, new String[5]);
 
-// Company Sınıfı için test senaryoları
-        String[] developers = new String[5];
+        // Employee'ler oluşturma
+        Employee ali = new Employee(1, "Ali Can", "ali@workintech.com", "password123", new String[1]);
+        Employee ayse = new Employee(2, "Ayşe Yılmaz", "ayse@workintech.com", "password456", new String[1]);
 
-        Company company1 = new Company(1,"Workintech", 152.0, new String[]{"Ali Can Sadeler"});
+        // Company'e Employee ekleme
+        System.out.println(workintech.addEmployee(0, ali.getFullName()));
+        System.out.println(workintech.addEmployee(1, ayse.getFullName()));
 
-        Company company2 = new Company(2, "Aselsan", -152.0, developers);
-            company2.addEmployee(0, "Ali Can Sadeler");
-            company2.addEmployee(5, "Hebele Hübele");
-            company2.addEmployee(1, "Hebele Hübele");
-            company2.addEmployee(0, "Hebele Hübele");
-        System.out.println(company1.toString("developerNames"));
-        System.out.println(company2.toString());
+        // Healthplan oluşturma
+        Healthplan basicPlan = new Healthplan(1, "Temel Sağlık Planı", Plan.BASIC);
 
-// healtplan sınıfı
+        // Employee'lere Healthplan ekleme
+        System.out.println(ali.addHealthPlan(0, basicPlan.getName()));
+        System.out.println(ayse.addHealthPlan(0, basicPlan.getName()));
 
-        Healthplan healthplan1 = new Healthplan(1, "Ali Can", Plan.BASIC);
-        System.out.println(healthplan1.toString());
+        // Sonuçları yazdırma
+        System.out.println("\nŞirket Bilgileri:");
+        System.out.println(workintech);
 
-// Employee sınıfı
+        System.out.println("\nÇalışan Bilgileri:");
+        System.out.println(ali);
+        System.out.println(ayse);
 
-        Employee employee1 = new Employee(1, "Ali Can Sadeler", "ali@gmail.com", "1234", new String[]{"BASIC"});
-        System.out.println(employee1.toString());
-
-
+        System.out.println("\nSağlık Planı Bilgileri:");
+        System.out.println(basicPlan);
     }
 }
